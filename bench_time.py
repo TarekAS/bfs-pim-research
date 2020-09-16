160654c34047119da6a119cd5f2bd6f6f7b8e74f
Tarek Abdel Sater <tarek.g.abdelsater@gmail.com>
2020-09-16 20:10:36 +0300

    improve benchmark script

--- bench_time.py
+++ bench_time.py
@@ -3,7 +3,7 @@
     Usage:
         For a single datafile:  $ python3 bench_time.py <datafile> <expected_node_levels>
         For multiple datafiles: $ python3 bench_time.py
-            Put the datafiles in ./data and the expected_node_levels in ./results/cpu.
+            Put the datafiles in ./data and the expected_node_levels in ./results/expected.
     
     The expected_node_levels are used to verify the correctness of the BFS output. 
 
@@ -94,51 +94,53 @@ def bfs(datafile, expected_node_levels, alg, prt, num_dpus):
 
 
 logging.basicConfig(filename='bench_time.error.log', level=logging.ERROR)
+
+# Compile code with 11 Tasklets and 32 bytes block size.
 make(11, 32)
 
-
 # (algorithm, partitioning) pairs
-algs = [("src", "row"), ("src", "col"), ("src", "2d"),
-        ("dst", "row"), ("dst", "col"), ("dst", "2d"),
+algs = [("top", "row"), ("top", "col"), ("top", "2d"),
+        ("bot", "row"), ("bot", "col"), ("bot", "2d"),
         ("edge", "row"), ("edge", "col"), ("edge", "2d")]
 
+# Get datafiles from args.
 datafiles = []
 if len(sys.argv) > 1:
     datafiles = [(sys.argv[1], sys.argv[2])]
 else:
     data = os.listdir("data")
-    expected = os.listdir("results/cpu")
+    expected = os.listdir("results/expected")
     missing = set(data) - set(expected)
     if len(missing) > 0:
         for f in missing:
             print(f"Missing expected output of file {f}")
         exit()
     for f in data:
-        datafiles.append((f"data/{f}", f"results/cpu/{f}"))
+        datafiles.append((f"data/{f}", f"results/expected/{f}"))
 
-min_dpus = 8
-max_dpus = 640
-
+# Create unique output file.
 outfile = "bench_time_results"
 counter = 2
 while os.path.isfile(outfile):
     outfile = f"bench_time_results_{counter}"
     counter += 1
-
 f = open(outfile, "w+")
 
+# Write header.
 f.write("datafile\talgorithm\tpartition\tsuccess\tnum_nodes\tnum_edges\tmax_degree_node\tmax_degree\tdpu_compute_time\thost_comm_time\tpop_mram_time\tfetch_res_time\ttotal_alg\ttotal_pop_fetch\ttotal_all\n")
 f.flush()
 
+# Run benchmarks on each datafile, for each combination of bfs variation and dpu count.
+dpu_count = [8, 16, 32, 64, 128, 256, 512]
 for datafile, expected in datafiles:
     for alg, prt in algs:
-        for num_dpus in range(min_dpus, max_dpus+8, 8):
+        for num_dpus in dpu_count:
 
             success, dpu_compute_time, host_comm_time, pop_mram_time, fetch_res_time, total_alg, total_pop_fetch, total_all = bfs(
                 datafile, expected, alg, prt, num_dpus)
             num_nodes, num_edges, max_degree_node, max_degree = get_metadata(
                 datafile)
 
-            f.write(f"{os.path.basename(datafile)}\t{alg}\t{prt}\t{num_dpus}\t{success}\t{num_nodes}\t{num_edges}\t{max_degree_node}\t{max_degree}\t{dpu_compute_time}\t{host_comm_time}\t{pop_mram_time}\t{fetch_res_time}\t{total_alg}\t{total_pop_fetch}\t{total_all}\n")
+            f.write(f"{os.path.basename(datafile)}\t{alg}\t{prt}\t{success}\t{num_dpus}\t{success}\t{num_nodes}\t{num_edges}\t{max_degree_node}\t{max_degree}\t{dpu_compute_time}\t{host_comm_time}\t{pop_mram_time}\t{fetch_res_time}\t{total_alg}\t{total_pop_fetch}\t{total_all}\n")
             f.flush()
 f.close()
