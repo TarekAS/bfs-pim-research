8579bb552d1ca03b6745d5cac4466f7aa616d880
Tarek Abdel Sater <tarek.g.abdelsater@gmail.com>
2020-09-26 17:57:04 +0300

    account for single direction data

--- format_tsv.sh
+++ format_tsv.sh
@@ -7,7 +7,16 @@ sed -i -r 's/(\s+)?\S+//3' $1
 sort -k1,1n -k2,2n <$1 > $1.tmp
 mv -f $1.tmp $1
 
-# Append number of nodes and number of edges to the  top.
-NUM_NODES=$(sort -nrk1,1 $1 | head -1 | awk '{print $1}')
+# Get number of nodes and edges.
 NUM_EDGES=$(wc -l $1  | awk '{print $1}')
+NUM_NODES_1=$(sort -nrk1,1 $1 | head -1 | awk '{print $1}')
+NUM_NODES_2=$(sort -nrk2,2 $1 | head -1 | awk '{print $2}')
+NUM_NODES=$NUM_NODES_1
+
+if [ $NUM_NODES_2 -gt $NUM_NODES_1 ]
+then
+  NUM_NODES=$NUM_NODES_2
+fi
+
+# Prepend number of nodes and number of edges.
 sed -i "1i$NUM_NODES $NUM_EDGES" $1
